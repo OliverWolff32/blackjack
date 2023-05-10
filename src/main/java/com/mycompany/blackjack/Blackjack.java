176713f61c21be9c59d977c5fb3a212776f188b1
Oliver Wolff <oliver.wolff@mrjaffesclass.com>
2023-05-10 12:42:35 -0700

    first

---
+++ src/main/java/com/mycompany/blackjack/Blackjack.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blackjack;
+
+/**
+ *
+ * @author student
+ */
+public class Blackjack {
+
+    public static void main(String[] args) {
+        View view = new View();
+        view.setVisible(true);
+    }
+}
